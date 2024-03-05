20112735c906603efbc179876362f9726f3a5fbb
Kate <katewolshanat@gmail.com>
2024-03-05 16:36:50 +0100

    eslint fixed

--- src/js/api/createRequest.js
+++ src/js/api/createRequest.js
@@ -1,4 +1,4 @@
-const createRequest = async ( {method, endpoint, body} ) => {
+const createRequest = async ({method, endpoint, body}) => {
     const response = await fetch(`https://backend-sse-ws.onrender.com/${endpoint}`, {
         method,
         body: JSON.stringify(body)
